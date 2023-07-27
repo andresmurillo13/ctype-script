d818480a0df0a587f7bb43946f247d755b72d3de
Andres Murillo <andresfelipemurillobonilla@gmail.com>
2023-07-27 07:46:40 -0500

    curso ts

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import { Hero } from './classes/Hero';
+// import { Hero as SuperHero, Hero2 } from './classes/Hero';
+// import * as HeroClasses from './classes/Hero';
+
+
+// const ironman = new SuperHero('Ironman', 1, 55);
+const ironman = new Hero('Ironman', 10, 55);
+
+console.log( ironman );
+console.log( ironman.power );
+
+
+
+
+
